4b831c7c25d2f8646919768a3aab4fb4f219d63c
tsion203 <tsionmira167@gmail.com>
2026-06-27 14:43:53 +0300

    Establish and check connection to the database

--- helpdesk/src/main/java/com/example/helpdesk/HelpDeskApplication.java
+++ helpdesk/src/main/java/com/example/helpdesk/HelpDeskApplication.java
@@ -1,13 +1,36 @@
 package com.example.helpdesk;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelpDeskApplication {
+public class HelpDeskApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpDeskApplication.class, args);
 	}
-
+	 @Override
+    public void run(String... args) throws Exception {
+        System.out.println("✅✅✅ WOOOHOOO! DATABASE CONNECTED SUCCESSFULLY! ✅✅✅");
+    }
 }
+
+
+/*import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectApplication.class, args);
+    }
+
+    // This code ONLY runs if the app and database start successfully!
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("✅✅✅ WOOOHOOO! DATABASE CONNECTED SUCCESSFULLY! ✅✅✅");
+    }
+} */
